b1500cb3ee54af5ad423d1b3c893b2293eea5a57
jiyuntu <wendy3925@gmail.com>
2023-01-20 23:45:07 +0800

    BuildCsvFromSuisiann

--- build_dataset.py
+++ build_dataset.py
@@ -23,7 +23,8 @@ class BuildCsv():
             chars_to_remove_regex = "[\,\?\.\!\;\:\"\“\%\‘\”\'\-]"
             batch["tailo"] = re.sub(chars_to_remove_regex, " ", batch["tailo"]).lower()
             # multiple spaces to one
-            batch["tailo"] = " ".join(batch["tailo"].split(" ")).strip()
+            tailo_list = batch["tailo"].split(" ")
+            batch["tailo"] = " ".join([w for w in tailo_list if w]).strip()
             return batch
         self.train_dataset = self.train_dataset.map(replace_with_space)
         self.test_dataset = self.test_dataset.map(replace_with_space)
@@ -31,6 +32,19 @@ class BuildCsv():
     def to_csv(self):
         self.train_dataset.to_csv(self.train_output_file, index=False)
         self.test_dataset.to_csv(self.test_output_file, index=False)
+
+    def tailo_transform(self, 台羅):
+        台羅物件 = 拆文分析器.建立句物件(台羅)
+        return 台羅物件.轉音(臺灣閩南語羅馬字拼音).看語句()
+
+    def is_valid_tailo(self, tailo):
+        # str isalnum is different from Alphabetic property
+        # https://docs.python.org/3/library/stdtypes.html#str.isalpha
+        return tailo.replace(" ", "").encode("utf-8").isalnum()
+
+    def remove_tone_instances(self):
+        self.train_dataset = self.train_dataset.filter(lambda example: self.is_valid_tailo(example["tailo"]))
+        self.test_dataset = self.test_dataset.filter(lambda example: self.is_valid_tailo(example["tailo"]))
 
 class BuildCsvFromCommonVoice(BuildCsv):
     def __init__(self):
@@ -42,27 +56,55 @@ class BuildCsvFromCommonVoice(BuildCsv):
     def tailo(self):
         # 台羅 to 台羅數字調
         # Some tone could not be transderred
-        def tailo_transform(batch):
+        def transform(batch):
             begin = batch["sentence"].find("（")
             # Try to find ｜ but often fail.
             end = batch["sentence"].find("）")
-            台羅 = batch["sentence"][begin + 1:end].strip()
-            台羅物件 = 拆文分析器.建立句物件(台羅)
-            batch["tailo"] = 台羅物件.轉音(臺灣閩南語羅馬字拼音).看語句()
+            batch["tailo"] = self.tailo_transform(batch["sentence"][begin + 1:end].strip())
             if "|" in batch["tailo"]:
                 batch["tailo"] = batch["tailo"].split("|")[0]
             return batch
         self.train_dataset = self.train_dataset.filter(lambda example: example["sentence"].find("（") >= 0)
         self.test_dataset = self.test_dataset.filter(lambda example: example["sentence"].find("（") >= 0)
 
-        self.train_dataset = self.train_dataset.map(tailo_transform)
-        self.test_dataset = self.test_dataset.map(tailo_transform)
+        self.train_dataset = self.train_dataset.map(transform)
+        self.test_dataset = self.test_dataset.map(transform)
 
     def build(self):
         self.tailo()
         self.remove_punctuation_marks()
+        self.remove_tone_instances()
+        self.to_csv()
+
+class BuildCsvFromSuisiann(BuildCsv):
+    def __init__(self):
+        super().__init__(["Suisiann/SuiSiann.csv"], ["Suisiann/SuiSiann.csv"],
+                          "Suisiann/SuiSiann-processed.csv", "Suisiann/SuiSiann-processed-2.csv")
+
+    def expand_filename(self):
+        def add_filename_prefix(batch):
+            batch["path"] = f"Suisiann/" + batch["path"]
+            return batch
+        self.train_dataset = self.train_dataset.map(add_filename_prefix)
+        self.test_dataset = self.test_dataset.map(add_filename_prefix)
+
+    def tailo(self):
+        def transform(batch):
+            batch["tailo"] = self.tailo_transform(batch["Rome"])
+            return batch
+        self.train_dataset = self.train_dataset.map(transform)
+        self.test_dataset = self.test_dataset.map(transform)
+
+    def build(self):
+        self.tailo()
+        self.remove_punctuation_marks()
+        self.remove_tone_instances()
+        self.expand_filename()
         self.to_csv()
 
 if __name__ == "__main__":
-    build_common_voice = BuildCsvFromCommonVoice()
-    build_common_voice.build()
+    # build_common_voice = BuildCsvFromCommonVoice()
+    # build_common_voice.build()
+
+    build_suisiann = BuildCsvFromSuisiann()
+    build_suisiann.build()
